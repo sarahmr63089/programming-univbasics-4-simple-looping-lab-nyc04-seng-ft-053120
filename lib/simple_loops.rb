45890356f38813cb66da867d55f2fcd9cfe9bc40
Sarah Rose <sarahrose63089@gmail.com>
2020-06-04 15:46:23 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -12,6 +12,13 @@ def output_array (array)
   array.each {|element| puts element}
 end
 
-def return_string_array
-
+def return_string_array (array)
+#This method should loop over the provided array, convert each element to a string, and return the new array of strings.
+  counter = 0
+  new_array = []
+  if array[counter]
+    new_item = array[counter].to_s
+    new_array << new_item
+    counter += 1
+  end
 end
